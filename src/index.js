a128670ce0e7f0ae750137c815cafaf57d12bf2b
alexaldearroyo <alexaldearroyo@gmail.com>
2023-11-05 03:24:39 +0900

    environments completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import "./styles.css";
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import $ from 'jquery';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import $ from "jquery";
 
-$(document).ready(function() {
-    $('#burgerMenu').click(function() {
-        console.log('Burger menu clicked!');
+$(function () {
+    $("#burgerMenu").on("click", function () {
+      var icon = $(this).find("i");
+  
+      if (icon.hasClass('rotated')) {
+          icon.removeClass('rotated');
+      } else {
+          icon.addClass('rotated');
+      }
+  
+      // Toggle la visibilidad de sideBarContainer
+      $("#sideBarContainer").toggle();
+      $("#contentBoxContainer").toggleClass("expanded");
     });
-});
+  });
+  
